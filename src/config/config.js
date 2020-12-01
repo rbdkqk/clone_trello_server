bd7e035546416abc00e1a8624f3e7445db562bd9
rbdkqk <rbdkqk@gmail.com>
2020-12-01 18:05:44 +0900

    [기능 구현] API 코드 작성 : 회원가입/로그인/로그아웃

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "trello",
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "trello",
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "trello",
+    host: process.env.DB_HOSTNAME,
+    port: 13306,
+    dialect: "mysql",
+  },
+};
---
+++ src/controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  authController: require("./auth"),
+  // userController: require("./user"),
+  // questionController: require("./question"),
+  // voteController: require("./vote"),
+};
